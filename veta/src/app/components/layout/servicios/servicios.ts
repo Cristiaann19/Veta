5feaca1acaac2c3b74d573580280b288ad0683fc
Chriso <huamancruzcristianjesus@gmail.com>
2026-02-07 20:18:28 -0500

    six-seven

--- veta/src/app/components/layout/servicios/servicios.ts
+++ veta/src/app/components/layout/servicios/servicios.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Servicio } from '../../../models/servicios';
+import { ServiciosService } from '../../../services/servicios';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PaginatorModule } from 'primeng/paginator';
 @Component({
   selector: 'app-servicios',
-  imports: [],
+  imports: [CommonModule, FormsModule, DialogModule, ButtonModule, InputTextModule, PaginatorModule],
   templateUrl: './servicios.html'
 })
-export class Servicios {
+export class Servicios implements OnInit {
+  servicios : Servicio[] = [];
+  serviciosFiltrados: Servicio[] = [];
+  serviciosMostrados: Servicio[] = [];
 
+  terminoBusqueda : string = '';
+
+  //modales
+  displayEdit : boolean = false;
+  displayDelete : boolean = false;
+  displayNew : boolean = false;
+
+  selectedServicio: Servicio = {} as Servicio;
+
+  //Paginacion
+  first: number = 0;
+  rows: number = 4;
+
+  constructor(private servicioService : ServiciosService, private cdr : ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.cargarServicios();
+  }
+
+  cargarServicios() : void{
+    this.servicioService.listar().subscribe({
+      next: (data) => {
+        this.servicios = data;
+        this.filtrar();
+        this.cdr.detectChanges();
+      },
+      error : (err) => console.error('Error: ', err)
+    })
+  }
+
+  filtrar(): void {
+    this.serviciosFiltrados = this.servicios.filter(v => 
+      v.nombre.toLocaleLowerCase().
+                includes(this.terminoBusqueda.toLocaleLowerCase()));
+    this.first = 0;
+    this.actualizarVista();
+  }
+  actualizarVista(): void {
+    this.serviciosMostrados = this.serviciosFiltrados.slice(this.first, this.first + this.rows);
+  }
 }
